09cac8430917c8267f10215178fd9e915e3a7353
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-28 15:23:44 +0100

    update classifiers

--- setup.py
+++ setup.py
@@ -3,15 +3,6 @@ from setuptools import setup, find_packages
 
 version = '1.1.dev0'
 
-classifiers = [
-    "Framework :: Plone",
-    "Framework :: Zope2",
-    "Programming Language :: Python",
-    "License :: OSI Approved :: GNU General Public License (GPL)",
-    "Topic :: Software Development :: Libraries :: Application Frameworks",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
-
 setup(
     name='collective.portlet.contact',
     namespace_packages=['collective', 'collective.portlet', ],
@@ -19,7 +10,20 @@ setup(
     description='Display LDAP contacts in Plone portlets.',
     long_description=open("README.rst").read() + "\n" +
                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    classifiers=classifiers,
+    classifiers=[
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Framework :: Zope2",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
     keywords='plone portlet ldap contact',
     author='Sylvain Boureliou',
     author_email='sylvain.boureliou@makina-corpus.com',
